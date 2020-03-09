c5e3f79b3976b01dec79cd3c6855e028d1c3ccef
Melissa Sperry <sperry.mel@gmail.com>
2020-03-09 03:19:37 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,22 +1,21 @@
 class EmailParser
-attr_accessor :emails
 
-def initialize(emails)
-  @emails = emails
+  attr_accessor :emails
+
+  def initialize(emails)
+    @emails = emails
+  end
+
+  def parse
+    email_array = []
+    email_array = @emails.split(/[,\s]/)
+    email_array = email_array.reject { |e| e == ""}
+    email_array.uniq
+  end
+
 end
 
-def parse
-  puts splitEmail = emails.split.collect {|address| address.split(',')}
-  returnArr = splitEmail.flatten.uniq
-  returnArr
-end
+emails = "hpotter@hogwarts.org, he_who_must_not_be_named@riddleshouse.net"
+parser = EmailParser.new(emails)
 
-end
-
-emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
-parser1 = EmailParser.new(emails1)
-parser1.parse
-
-emails2 = "asd@qw.com per@er.org per@er.org Zaza@sugar.net"
-parser2 = EmailParser.new(emails2)
-parser2.parse
+puts parser.parse
